790e237a2fbb5aec122648eecf21a0fc57dd74d5
Ryley Herrington <ryleyherrington@gmail.com>
2013-04-08 00:39:31 -0700

    got rid of repeat folders

--- app-hackathon/settings.py
+++
@@ -1,8 +0,0 @@
-"""Configuration constants for the app-hackacthon software.
-
-Author: Dan Albert <dan@gingerhq.net>
-"""
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-TEMPLATE_DIR = 'templates'
